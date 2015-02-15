d3b5fc7fc7ffef423aad2d4508a7593b2c358ad7
barry <barry@staes.nl>
2015-02-15 16:55:19 +0100

    eeprom 2nd pseudocode

--- Arduino_Tests/eeprom_test/eeprom_test/eeprom_test.ino
+++ Arduino_Tests/eeprom_test/eeprom_test/eeprom_test.ino
@@ -1,6 +1,7 @@
 /* LoadAndSaveSettings
- * Joghurt 2010
+ * footswitch 2012-03-05, original code by Joghurt (2010)
  * Demonstrates how to load and save settings to the EEPROM
+ * Tested on Arduino Uno R2 with Arduino 0023
  */
 // Contains EEPROM.read() and EEPROM.write()
 #include <EEPROM.h>
@@ -13,36 +14,52 @@
 
 // Example settings structure
 struct StoreStruct {
-  // This is for mere detection if they are your settings
-  char version[4];
   // The variables of your settings
   int a, b;
   char c;
   long d;
   float e[6];
-} storage = {
-  CONFIG_VERSION,
+  // This is for mere detection if they are your settings
+  char version_of_program[4]; // it is the last variable of the struct
+  // so when settings are saved, they will only be validated if
+  // they are stored completely.
+} settings = {
   // The default values
   220, 1884,
   'c',
   10000,
-  {4.5, 5.5, 7, 8.5, 10, 12}
+  {4.5, 5.5, 7, 8.5, 10, 12},
+  CONFIG_VERSION
 };
 
 void loadConfig() {
   // To make sure there are settings, and they are YOURS!
   // If nothing is found it will use the default settings.
-  if (EEPROM.read(CONFIG_START + 0) == CONFIG_VERSION[0] &&
-      EEPROM.read(CONFIG_START + 1) == CONFIG_VERSION[1] &&
-      EEPROM.read(CONFIG_START + 2) == CONFIG_VERSION[2])
-    for (unsigned int t=0; t<sizeof(storage); t++)
-      *((char*)&storage + t) = EEPROM.read(CONFIG_START + t);
+  if (//EEPROM.read(CONFIG_START + sizeof(settings) - 1) == settings.version_of_program[3] // this is '\0'
+      EEPROM.read(CONFIG_START + sizeof(settings) - 2) == settings.version_of_program[2] &&
+      EEPROM.read(CONFIG_START + sizeof(settings) - 3) == settings.version_of_program[1] &&
+      EEPROM.read(CONFIG_START + sizeof(settings) - 4) == settings.version_of_program[0])
+  { // reads settings from EEPROM
+    for (unsigned int t=0; t<sizeof(settings); t++)
+      *((char*)&settings + t) = EEPROM.read(CONFIG_START + t);
+  } else {
+    // settings aren't valid! will overwrite with default settings
+    saveConfig();
+  }
 }
 
 void saveConfig() {
-  for (unsigned int t=0; t<sizeof(storage); t++)
-    EEPROM.write(CONFIG_START + t, *((char*)&storage + t));
+  for (unsigned int t=0; t<sizeof(settings); t++)
+  { // writes to EEPROM
+    EEPROM.write(CONFIG_START + t, *((char*)&settings + t));
+    // and verifies the data
+    if (EEPROM.read(CONFIG_START + t) != *((char*)&settings + t))
+    {
+      // error writing to EEPROM
+    }
+  }
 }
+
 
 void setup() {
   loadConfig();
@@ -50,12 +67,12 @@ void setup() {
 
 void loop() {
   // [...]
-  int i = storage.c - 'a';
+  int i = settings.c - 'a';
   // [...]
 
   // [...]
-  storage.c = 'a';
-  if (ok)
+  settings.c = 'a';
+  if (some_condition)
     saveConfig();
   // [...]
 }
