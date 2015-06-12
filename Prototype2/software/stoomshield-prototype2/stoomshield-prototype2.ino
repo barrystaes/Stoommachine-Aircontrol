6272467b925dce8609517c9617d643dc6bdf47a8
Barry Staes <barry@staes.nl>
2015-06-12 22:47:04 +0200

    Prototype2 software: Added UTFT and test render.

--- Prototype2/software/stoomshield-prototype2/stoomshield-prototype2.ino
+++ Prototype2/software/stoomshield-prototype2/stoomshield-prototype2.ino
@@ -6,23 +6,46 @@
 
 // Includes
 #include <DueTimer.h>
+#include <UTFT.h>
+extern uint8_t SmallFont[];
+extern uint8_t BigFont[];
 
 // Configure logging
 const int SensorReadsPerSecond = 1000; // setting
 
-void setup() {
-  // TODO setup display
+// Variables
+int rpm = 0;
 
+
+UTFT myGLCD(SSD1289,40,41,38,39);
+
+void setup() {
+  // Setup display
+  
+  // Draw backdrop
+  myGLCD.InitLCD();
+  myGLCD.clrScr();
+  myGLCD.setFont(BigFont);
+  myGLCD.setColor(255, 255, 255);
+  myGLCD.print("StoomShield-proto.2", CENTER, 0);
+  
   // Sensor timer
   Timer2.attachInterrupt(timer_SensorRead).setFrequency(SensorReadsPerSecond).start();
 }
 
 void timer_SensorRead() {
   // TODO read sensors
+  rpm++; // test
 }
 
 void loop() {
   // This is the render loop
+  
+  myGLCD.setFont(SmallFont);
+  myGLCD.print("RPM", 0, 20);
+  myGLCD.setFont(BigFont);
+  myGLCD.printNumI(rpm, 24, 16);
+  
   
   // TODO render display
 
